79cfe11c4e376762742f644df10e0d694f710689
Simran <101885248+67simran@users.noreply.github.com>
2022-04-01 07:50:38 +0530

    Add files via upload

---
+++ 1. Medium 1/India temp time series.R
@@ -0,0 +1,32 @@
+#GSoC 2022
+#ggplot
+#Author: Simran
+#Name: RSQLiteAdmin Visualisation Tools
+
+#Plot a time-series line chart on a dataset of your choice with different variables in a single chart. Customize it to make it clear and insightful.
+
+     
+     library(ggplot2)
+     library(readxl)
+     tas_timeseries_annual_cru_1901_2020_IND <- read_excel("C:\\Users\\visha\\Downloads\\tas_timeseries_annual_cru_1901-2020_IND.xls")
+     data.frame(tas_timeseries_annual_cru_1901_2020_IND)
+     head(tas_timeseries_annual_cru_1901_2020_IND)
+     
+     
+     tas_timeseries_annual_cru_1901_2020_IND$Time
+
+     # view the time column
+     tas_timeseries_annual_cru_1901_2020_IND$India
+     
+     
+     qplot(x = tas_timeseries_annual_cru_1901_2020_IND$Time,
+           y = tas_timeseries_annual_cru_1901_2020_IND$India)
+     
+     # plotting the data
+     ggplot( data = tas_timeseries_annual_cru_1901_2020_IND, aes(x= tas_timeseries_annual_cru_1901_2020_IND$Time, y= tas_timeseries_annual_cru_1901_2020_IND$India  )) + 
+       geom_line() +
+       labs(x = "Year",
+            y = "Temperature (C)",
+            title = "Annual average temperature",
+            subtitle = "India 1901-2020")
+     
